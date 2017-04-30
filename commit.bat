3b22983de5c4715aad0c132df85d2eca37e94191
Jayson Nabor <sonabstudios@gmail.com>
2017-04-30 12:00:00 +0800

    commit on 2017-04-30

---
+++ commit.bat
@@ -0,0 +1,9 @@
+set arg1=%1
+set arg2=%2
+echo "Executing commit hack script"
+echo "creating a file"
+echo "Hello World on %arg1%" > %arg1%.log
+ECHO new file added on %arg1% >>"README.md"
+git add .
+git commit --date="%arg1% %arg2%" -m "commit on %arg1%"
+@rem date="$Y-$M-$D 12:00:00"
